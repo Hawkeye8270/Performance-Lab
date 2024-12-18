018e42f09162514d4b196d0544e390de70cb4d81
Hawkeye8270 <143038811+Hawkeye8270@users.noreply.github.com>
2024-12-18 22:11:01 +0300

    Add files via upload

---
+++ Task1/target/classes/org/example/task1.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class task1 {
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+        int m = Integer.parseInt(args[1]);
+
+
+        int[] mass = new int[n];
+        for (int i = 0; i < n; i++) {
+            mass[i] = i + 1;
+        }
+
+        int temp = 0;
+
+        do {
+            System.out.print(mass[temp]);
+            temp = (temp + m - 1) % n;
+        } while (temp != 0);
+    }
+}
+
